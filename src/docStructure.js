8684986963bfa382d66fc5ca68864cbd40565a64
steve omari <steveomari62@gmail.com>
2023-02-15 20:00:03 -0500

    setup header structure

--- src/docStructure.js
+++ src/docStructure.js
@@ -3,17 +3,25 @@ const doc = (() => {
     const header= document.createElement("header");
     const nav = document.createElement("nav");
     const main = document.createElement("main");
+    const headerTitle= document.createElement("div");
+    const headerBtn= document.createElement("button");
 
     pageContainer.classList.add("page-container");
+    headerTitle.classList.add("header-title");
+    headerBtn.classList.add("header-btn");
 
-    header.textContent="header";
     nav.textContent = "nav";
     main.textContent = "main";
+    headerTitle.textContent="Todo List";
+    headerBtn.textContent="Add"
+
 
     document.body.appendChild(pageContainer);
     pageContainer.appendChild(header);
     pageContainer.appendChild(nav);
     pageContainer.appendChild(main);
+    header.appendChild(headerTitle);
+    header.appendChild(headerBtn);
 })();
 
 export default doc;
